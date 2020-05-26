5819eda3d638cf1671dc8deb82dacbf9653ab3c7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-26 12:34:37 -0500

    [+] NPM > @emotion(core/styled), gatsby-plugin-emotion: Defines the global style sheet of the project

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 /** Dependencies */
 import { Helmet } from 'react-helmet';
+import { Global, css } from '@emotion/core';
 
 /** Components */
 import Header from './Header';
@@ -10,6 +11,33 @@ import Header from './Header';
 const MainLayout = ( props ) => {
     return (
         <>
+            <Global
+                styles={ css `
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 16px;
+                        font-size: 1.6rem;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3, h4, h5, h6 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    h1, h2, h3 {
+                        line-height: 1.5;
+                        margin: 0;
+                    }
+                    ul {
+                        margin: 0;
+                        list-style: none;
+                        padding: 0;
+                    }
+                `}
+            />
             <Helmet>
                 <title>Gatsby Hotel</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
