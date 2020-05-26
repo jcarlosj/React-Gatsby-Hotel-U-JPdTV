23c3c580e1046fc59320b7cf079efa9f1fb40a6e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-26 11:36:28 -0500

    [+] Component > MainLayout: Customize document head to add Normalize and Google Fonts

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+/** Dependencies */
+import { Helmet } from 'react-helmet';
+
 /** Components */
 import Header from './Header';
 
@@ -7,6 +10,12 @@ import Header from './Header';
 const MainLayout = ( props ) => {
     return (
         <>
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
+            </Helmet>
+
             <Header />
             { props .children }
         </>
