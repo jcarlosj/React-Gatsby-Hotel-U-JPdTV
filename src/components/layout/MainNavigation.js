3881bb56fe405d4256ea49430af4a711445339c0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-26 15:12:29 -0500

    [+] Component > MainNavigation: Add CSS styles using emotion

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,14 +3,51 @@ import { Link } from 'gatsby';
 
 /** Dependencies */
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+
+/** Style Component */
+const 
+    Nav = styled .nav `
+        display: flex;
+        justify-content: center;
+        padding-botton: 3rem;
+
+        @media ( min-width: 768px ) {
+            padding-bottom: 0;
+        }
+    `,
+    NavLink = styled( Link ) `
+        color: white;
+        font-family: 'PT Sans', sans-serif;
+        font-size: 1.6rem;
+        font-weight: 700;
+        line-height: 1rem;
+        margin-right: 1rem;
+        padding: 1rem;
+        text-decoration: none;
+
+        &:last-of-type {
+            margin-right: 0;
+        }
+
+        &.current-page {
+            border-bottom: 2px solid white;
+        }
+    `;
 
 /** MainNavigation Component */
 const MainNavigation = () => {
     return (
-        <nav>
-            <Link to="/" >Inicio</Link>
-            <Link to="/us" >Nosotros</Link>
-        </nav>
+        <Nav>
+            <NavLink 
+                to="/" 
+                activeClassName="current-page"
+            >Inicio</NavLink>
+            <NavLink 
+                to="/us" 
+                activeClassName="current-page"
+            >Nosotros</NavLink>
+        </Nav>
     );
 }
 
