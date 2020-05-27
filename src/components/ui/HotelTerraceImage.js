227f8e633319982a342c826c7899bee976537afd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-27 10:51:38 -0500

    [+] NPM > gatsby-background-image: Install and implement the image display obtained from query with GraphQL

--- src/components/ui/HotelTerraceImage.js
+++ src/components/ui/HotelTerraceImage.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
+/** Dependencies */
+import styled from '@emotion/styled';
+import BackgroundImage from 'gatsby-background-image';
+
+/** Style Components */
+const BgImage = styled( BackgroundImage ) `
+    height: 700px;
+`;
+
 /** HotelTerraceImage Component */
 const HotelTerraceImage = () => {
 
-    /** Consulta GraphQL para obtener imagen estática */ 
+    /** Consulta GraphQL para obtener imagen estática 
+     *      - Agregar Fragment '...GatsbyImageSharpFluid_withWebp' 
+     *        (No es soportado por GraphiQL, pero Gasttby si)*/ 
     const { image } = useStaticQuery( graphql `
         query {
             image: file( relativePath: { eq: "hotel-terrace.jpg" } )
             {
                 sharp: childImageSharp {
                     fluid {
-                        srcSetWebp
+                        ...GatsbyImageSharpFluid_withWebp
                     }
                 }
             }
         }
     `);
 
-    console .log( 'GraphQL - HotelTerraceImage', image );
+    console .log( 'GraphQL - HotelTerraceImage', image .sharp .fluid );
 
     return (
-        <>
-            Imagen - HotelTerraceImage
-        </>
+        <BgImage
+            tag="section"
+            fluid={ image .sharp .fluid }
+            fadeIn="soft"
+        >
+        
+        </BgImage>
     );
 }
 
