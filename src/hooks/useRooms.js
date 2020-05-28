7139011167a62218723cbe606bfc4b28ba9795a1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-28 13:50:43 -0500

    [+] Hook > useRooms: Get rooms using a Hook to query with GraphQL at home

---
+++ src/hooks/useRooms.js
@@ -0,0 +1,35 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+/** Hook */
+const useRooms = () => {
+
+    /** Consulta GraphQL */ 
+    const data = useStaticQuery( graphql `
+        query {
+            allDatoCmsRoom {
+                nodes {
+                    title
+                    slug
+                    content
+                    image {
+                        fluid( maxWidth: 1200 ) {
+                            ...GatsbyDatoCmsFluid
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    console .log( 'Hook > useRooms', data );
+
+    return data .allDatoCmsRoom .nodes .map( room => ({
+        id: room .id,
+        title: room .title,
+        slug: room .slug,
+        content: room .content,
+        image: room .image
+    }));
+}
+
+export default useRooms;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,21 @@ import MainLayout from '../components/layout/MainLayout';
 import HotelTerraceImage from '../components/ui/HotelTerraceImage';
 import ContentHomePage from '../components/ui/ContentHomePage';
 
+/** Hooks */
+import useRooms from '../hooks/useRooms';
+
 /** Component */
-const IndexPage = ( props ) => (
-    <MainLayout>
-        <HotelTerraceImage />
-        <ContentHomePage />
-    </MainLayout>
-)
+const IndexPage = ( props ) => {
+
+    const rooms = useRooms();
+    console .log( 'IndexPage > rooms', rooms );
+
+    return(
+        <MainLayout>
+            <HotelTerraceImage />
+            <ContentHomePage />
+        </MainLayout>
+    );
+}
 
 export default IndexPage
